3e3092e44ceed50074226592f97a06d6a55c5366
Sofia <sofia.stazi04@gmail.com>
2024-10-25 18:09:28 -0300

    solución

--- app.js
+++ app.js
@@ -38,12 +38,25 @@ app.put("/people/:index", (req, res) => {
   /* COMPLETA EL CÓDIGO NECESARIO:
      Para que se pueda actualizar el objeto asociado al índice indicado en la URL 
    */
+  const personIndex = parseInt(req.params.index); // Pasamos el parámetro index a un entero
+  const updatedPerson = req.body; // Crea una constante con la información actualizada que se le da en el cuerpo de la solicitud 
+  
+  if (personIndex < 0 || personIndex >= people.length) return res.status(404).send ({ message: 'Persona no encontrada.'}); // Si no encuentra ninguna persona con ese índice, devuelve un error
+
+  people[personIndex] = { ...people[personIndex], ...updatedPerson }; // Actualiza el la persona con la información nueva
+  res.json(people[personIndex]); // Le respondemos al cliente el objeto añadido y actualizado
 });
 
 app.delete("/people/:index", (req, res) => {
   /* COMPLETA EL CÓDIGO NECESARIO:
      Para que se pueda eliminar el objeto asociado al índice indicado en la URL 
    */
+    const personIndex = parseInt(req.params.index); // Convertimos en entero el índice que nos pasan en el parámetro
+
+    if (personIndex < 0 || personIndex >= people.length) return res.status(404).send ({ message: 'Persona no encontrada.'}); // Si no encuentra a la persona devuelve un mensaje de error
+
+    people.splice(personIndex, 1); // Si la encuentra, elimina una persona desde el lugar del index proporcionado (o sea a la persona con ese index)
+    res.status(200).send({ message: 'Persona eliminada correctamente.'}); // Devuelve un mensaje para confirmar que la persona se eliminó correctamente
 });
 
 // Esta línea inicia el servidor para que escuche peticiones en el puerto indicado
